b4d8b19783e690cb071db54720224017eedea158
Vinit Shah <elysiums.shade@gmail.com>
2013-04-27 20:48:07 -0500

    Cleanup pre-Sankey-fying methods/vars

--- scripts/main.js
+++ scripts/main.js
@@ -23,14 +23,10 @@ var candidates = [],
 
 // Links
 var industryToCandidates = [],
-    candidatesToVotes    = [],
-    d3color              = d3.interpolateRgb("#BAE4B3", "#006D2C"),
-    d3LineLinear         = d3.svg.line().interpolate("linear");
+    candidatesToVotes    = [];
 
 // Drawing Helper Functions
-var color_scale    = null,
-    strength_scale = null,
-    path           = null,
+var path           = null,
     formatNumber   = null,
     format         = null,
     color          = null;
@@ -137,25 +133,6 @@ function drawNodes(nodeData, nodeClass)
                 {
                     return d.name + "\n" + format(d.value);
                 })
-        /*
-                .attr("id", function(node)
-                {
-                    return id_pre + node.id;
-                })
-                .attr("class", nodeClass)
-                .attr("cx", function(cand)
-                {
-                    return x;
-                })
-                .attr("cy", function(d, i)
-                {
-                    return i*25 + 20;
-                })
-                .attr("r", 0)
-            .transition()
-                .duration(1000)
-                .attr("r", radius);
-        */
 }
 
 function drawConnections(connectionData, connectionClass)
@@ -180,115 +157,6 @@ function drawConnections(connectionData, connectionClass)
                                 d.target.name + "\n" +
                                 format(d.value);
                     });
-    /*
-        .data(connectionData)
-        .enter().append("path")
-            .attr("class", connectionClass)
-            .attr("id", function(node, i)
-            {
-                return id_pre + i;
-            })
-            .attr("fill", function(d)
-            {
-                return d3color(color_scale(0));
-            })
-            .attr("d", function(d)
-            {
-                return drawCurveStart(d, src_pre, dst_pre);
-            })
-            .attr("stroke-width", 0)
-        .transition()
-            .duration(1000)
-            .attr("stroke-width", 1.5)
-            .attr("d", function(d)
-            {
-                return drawCurve(d, src_pre, dst_pre);
-            })
-            .attr("fill", function(d)
-            {
-                return d3color(color_scale(d.percent));
-            });
-    */
-}
-
-function drawCurveStart(d, src_pre, target_pre)
-{
-    // Inspired by this: http://blog.stephenboak.com/2012/06/15/d3-flow-vis-tutorial.html
-    var source_id = src_pre + d.source;
-    var target_id = target_pre + d.target
-    var slope = Math.atan2((+d3.select(target_id).attr("cy") - d3.select(source_id).attr("cy")),
-                           (+d3.select(target_id).attr("cx") - d3.select(source_id).attr("cx")));
-    var slopePlus90 = slope + (Math.PI / 2);
-
-    var sourceX = +d3.select(source_id).attr("cx");
-    var sourceY = +d3.select(source_id).attr("cy");
-    var targetX = +d3.select(target_id).attr("cx");
-    var targetY = +d3.select(target_id).attr("cy");
-
-    var arrowOffset = 20;
-    var points = [];
-    var xTan = radius*Math.cos(slope)
-    ,   xOff = radius*(d.percent/100)*Math.cos(slopePlus90)
-    ,   yTan = radius*Math.sin(slope)
-    ,   yOff = radius*(d.percent/100)*Math.sin(slopePlus90);
-
-    var first = [sourceX , sourceY]
-    ,   second = [sourceX, sourceY]
-    ,   third = [sourceX, sourceY];
-    /*var first = [sourceX + radius * Math.cos(slope) - d.percent * Math.cos(slopePlus90), sourceY + radius * Math.sin(slope) - (d.percent) * Math.sin(slopePlus90)]
-    ,   second = [sourceX + radius * Math.cos(slope), sourceY + radius * Math.sin(slope)]
-    ,   third = [targetX - radius * Math.cos(slope), targetY - radius * Math.sin(slope)]
-    ,   fourth = [targetX - (radius + arrowOffset) * Math.cos(slope) - (d.percent + (arrowOffset / 2)) * Math.cos(slopePlus90), targetY - (radius + arrowOffset) * Math.sin(slope) - (d.percent+ (arrowOffset / 2)) * Math.sin(slopePlus90)]
-    ,   fifth = [targetX - (radius + arrowOffset) * Math.cos(slope) - (d.percent) * Math.cos(slopePlus90), targetY - (radius + arrowOffset) * Math.sin(slope) - (d.percent) * Math.sin(slopePlus90)];
-    */
-    points.push(first);
-    points.push(second);
-    points.push(third);
-    /*points.push(third);
-    points.push(fourth);
-    points.push(fifth);*/
-
-    return d3LineLinear(points) + "Z";
-}
-
-function drawCurve(d, src_pre, target_pre)
-{
-    // Inspired by this: http://blog.stephenboak.com/2012/06/15/d3-flow-vis-tutorial.html
-    var source_id = src_pre + d.source;
-    var target_id = target_pre + d.target
-    var slope = Math.atan2((+d3.select(target_id).attr("cy") - d3.select(source_id).attr("cy")),
-                           (+d3.select(target_id).attr("cx") - d3.select(source_id).attr("cx")));
-    var slopePlus90 = slope + (Math.PI / 2);
-
-    var sourceX = +d3.select(source_id).attr("cx");
-    var sourceY = +d3.select(source_id).attr("cy");
-    var targetX = +d3.select(target_id).attr("cx");
-    var targetY = +d3.select(target_id).attr("cy");
-
-    var arrowOffset = 20;
-    var points = [];
-    var xTan = radius*Math.cos(slope)
-    ,   xOff = radius*(d.percent/100)*Math.cos(slopePlus90)
-    ,   yTan = radius*Math.sin(slope)
-    ,   yOff = radius*(d.percent/100)*Math.sin(slopePlus90);
-
-    var first = [sourceX + xOff, sourceY + yOff]
-    ,   second = [sourceX - xOff, sourceY - yOff]
-    ,   third = [targetX, targetY];
-    /*var first = [sourceX + radius * Math.cos(slope) - d.percent * Math.cos(slopePlus90), sourceY + radius * Math.sin(slope) - (d.percent) * Math.sin(slopePlus90)]
-    ,   second = [sourceX + radius * Math.cos(slope), sourceY + radius * Math.sin(slope)]
-    ,   third = [targetX - radius * Math.cos(slope), targetY - radius * Math.sin(slope)]
-    ,   fourth = [targetX - (radius + arrowOffset) * Math.cos(slope) - (d.percent + (arrowOffset / 2)) * Math.cos(slopePlus90), targetY - (radius + arrowOffset) * Math.sin(slope) - (d.percent+ (arrowOffset / 2)) * Math.sin(slopePlus90)]
-    ,   fifth = [targetX - (radius + arrowOffset) * Math.cos(slope) - (d.percent) * Math.cos(slopePlus90), targetY - (radius + arrowOffset) * Math.sin(slope) - (d.percent) * Math.sin(slopePlus90)];
-	*/
-    points.push(first);
-    points.push(second);
-    points.push(third);
-    /*points.push(third);
-    points.push(fourth);
-    points.push(fifth);*/
-
-    return d3LineLinear(points) + "Z";
 }
 
 function dragmove(d)
@@ -321,18 +189,6 @@ function initLayers()
 
 function setupFunctions()
 {
-    color_scale = d3.scale.linear()
-    .range([0, 1])
-    .domain([0, d3.max(industryToCandidates, function(d) {
-        return d.percent;
-    })]);
-
-    //GLOBAL STRENGTH SCALE
-    strength_scale = d3.scale.linear().range([2, 10]) /* thickness range for flow lines */
-    .domain([0, d3.max(industryToCandidates, function(d) {
-        return d.percent;
-    })]);
-
     formatNumber = d3.format(",.0f");
     format = function(d) { return "$" + formatNumber(d); };
     color = d3.scale.category20();
@@ -366,12 +222,6 @@ function setup()
 
     // Setup functions
     setupFunctions();
-
-    /*drawNodes(votes, "vote");
-    drawNodes(candidates, "candidate");
-    drawNodes(industries, "industry");
-
-    drawConnections(industryToCandidates, "ind2cand");*/
 
     drawSankey();
 }
